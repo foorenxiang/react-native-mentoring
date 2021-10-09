893f5c3831e5205de1bf53cae0bfbd22250d525f
Foo Ren Xiang <foorenxiang@gmail.com>
2021-10-09 18:36:03 +0800

    change root path behaviour to react to user's logged in state

---
+++ react-fundamentals/courses/src/Components/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+
+const Home = () => {
+  const userToken = (async () => {
+    const userTokenLoaded = await JSON.parse(localStorage.getItem("userToken"));
+    return userTokenLoaded;
+  })();
+
+  return userToken ? <Redirect to="/courses/" /> : <Redirect to="/login" />;
+};
+
+export default Home;
--- react-fundamentals/courses/src/Routes.jsx
+++ react-fundamentals/courses/src/Routes.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import Header from './Components/Header';
-import Login from './Components/Login';
-import Registration from './Components/Registration';
-import Courses from './Components/Courses';
-import CreateCourse from './Components/Courses/CreateCourse';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Components";
+import Header from "./Components/Header";
+import Login from "./Components/Login";
+import Registration from "./Components/Registration";
+import Courses from "./Components/Courses";
+import CreateCourse from "./Components/Courses/CreateCourse";
+import SampleCourseInfo from "./Components/Courses/DraftCourseInfo";
 
 const Routes = () => (
   <BrowserRouter>
@@ -12,9 +14,10 @@ const Routes = () => (
     <Switch>
       <Route path="/login" component={Login} />
       <Route path="/registration" component={Registration} />
-      <Route exact path="/courses/" component={Courses} />
+      <Route exact path="/courses" component={Courses} />
+      <Route exact path="/courses/:course" component={SampleCourseInfo} />
       <Route exact path="/courses/add" component={CreateCourse} />
-      <Route path="/" component={() => <Redirect to="/courses/" />} />
+      <Route path="/" component={Home} />
     </Switch>
   </BrowserRouter>
 );
